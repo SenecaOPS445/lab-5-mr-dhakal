b8c968eb8108b0d486642ec90c72d898789183ed
Manish Raj Dhakal <mrdhakal@myseneca.ca>
2024-08-08 13:12:33 -0400

    Submitting lab 5

--- lab5a.py
+++ lab5a.py
@@ -2,35 +2,25 @@
 # Author ID: mrdhakal
 
 def read_file_string(file_name):
-    # Takes file_name as string for a file name, returns its entire contents as a string
-    
-    f = open('data.txt', 'r')
-    text = f.read()
-    f.close()
-    
-    return text
-
+    """
+    Takes file_name as a string and returns the entire contents of the file as a string.
+    """
+    file = open(file_name, 'r')
+    content = file.read()
+    file.close()
+    return content
 
 def read_file_list(file_name):
-    # Takes a file_name as string for a file name, 
-
-    f = open('data.txt', 'r')
-    li = f.readlines()
-    f.close()
-
-    result = []
-    for line in li:
-        if line[-1] == '\n':
-            result.append(line[:-1])
-        else:
-            result.append(line)
-    
-    
-    # return its entire contents as a list of lines without new-line characters
-    return result
+    """
+    Takes file_name as a string and returns the entire contents of the file as a list of lines
+    without new-line characters.
+    """
+    file = open(file_name, 'r')
+    lines = [line.strip() for line in file]
+    file.close()
+    return lines
 
 if __name__ == '__main__':
     file_name = 'data.txt'
     print(read_file_string(file_name))
     print(read_file_list(file_name))
-pass
--- lab5c.py
+++ lab5c.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python3
-# Author ID: [seneca_id]
+# Author ID: mrdhakal
 
 def add(number1, number2):
     # Add two numbers together, return the result, if error return string 'error: could not add numbers'
     try:
-        add_num = int(number1) + int(number2)
-        return add_num
-    except Exception as e:
-        return e
-
-
+        result = int(number1) + int(number2)
+        return result
+    except:
+        return 'error: could not add numbers'
 
 def read_file(filename):
     # Read a file, return a list of all lines, if error return string 'error: could not read file'
-
     try:
-        file = open(filename, 'r')
+        file = open(filename, 'r') 
         lines = file.readlines()
         return lines
-    except Exception as e:
-        return e
-
+    except FileNotFoundError:
+        return 'error: could not read file'
 
 if __name__ == '__main__':
-    print(add(10,5))                        # works
-    print(add('10',5))                      # works
-    print(add('abc',5))                     # exception
-    print(read_file('seneca2.txt'))         # works
-    print(read_file('file10000.txt'))       # exception
+    print(add(10, 5))                        # works
+    print(add('10', 5))                      # works
+    print(add('abc', 5))                     # exception
+    print(read_file('seneca2.txt'))          # works
+    print(read_file('file10000.txt'))        # exception
